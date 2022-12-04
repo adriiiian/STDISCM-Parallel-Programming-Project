584a6ab9d0633ddd8571e8f9378c4995d0d2ebbb
Adrian Jerez <69713328+adriiiian@users.noreply.github.com>
2022-12-04 15:58:00 +0800

    added processes implementation

--- main.py
+++ main.py
@@ -2,8 +2,55 @@ import os, sys
 import glob
 from PIL import Image
 from PIL import ImageEnhance
+import time, random, multiprocessing
+
+class append_image(multiprocessing.Process):
+    def __init__(self, count, queue, image_files):
+        multiprocessing.Process.__init__(self)
+        self.counter=int(count)
+        self.list=[]
+        self.item=0
+        self.queue=queue
+        self.image_files = image_files
+
+    def run(self):
+        for i in self.image_files:
+            self.queue.put(i)
+            
+class process_class(multiprocessing.Process):
+    def __init__(self, count, process_ID, queue):
+        multiprocessing.Process.__init__(self)
+        self.counter=int((count/num_process))
+        self.list=[]
+        self.item=0
+        self.ID=process_ID
+        self.queue=queue
+        self.brightness = brightness
+        self.sharpness = sharpness
+        self.contrast = contrast
+        self.output_directory = output_directory
+
+    def run(self):
+        for i in range(self.counter):
+            self.item=self.queue.get()
+            self.list.append(self.item)
+            image_enhanced = Image.open(self.item)
+            fn = str(image_enhanced.filename)
+            brightness_img = ImageEnhance.Brightness(image_enhanced)
+            image_enhanced = brightness_img.enhance(self.brightness)
+            sharpness_img = ImageEnhance.Sharpness(image_enhanced)
+            image_enhanced = sharpness_img.enhance(self.sharpness)
+            contrast_img = ImageEnhance.Contrast(image_enhanced)
+            image_enhanced = contrast_img.enhance(self.contrast)
+            image_enhanced.show()
+            dump = fn.split("\\")
+            fn = dump[-1]
+            image_enhanced.save(f"{self.output_directory}{fn}")
 
 if __name__=="__main__":
+
+    queue = multiprocessing.Queue()
+    process_list = []
 
     input_directory = input("Enter the folder of input directory: ")
     #print(input_directory)
@@ -25,8 +72,8 @@ if __name__=="__main__":
     contrast = float(input("Enter the contrast enhancement factor: "))
     #print(contrast)
 
-    num_threads = int(input("Enther the number of threads/process to use: "))
-    #print(num_threads)
+    num_process = int(input("Enther the number of threads/process to use: "))
+    #print(num_process)
 
     list_directory = []
     extensions = ('/*.jpg', '/*.png', '/*.gif')
@@ -42,20 +89,18 @@ if __name__=="__main__":
     #Getting all files that has the extensions .jpg .png .gif in the source folder and storing it in the list
     for files in list_directory:
         image_files.extend(glob.glob(files))
+        
+    produce = append_image(len(image_files), queue, image_files)
+    produce.start()
+
+    for i in range(int(num_process)):
+        process = process_class(len(image_files), i, queue)
+        process_list.append(process)
+        process.start()
+
+    for p in process_list:
+        p.join()
 
 
-    #Enhancing images inside the list and saving to destination folder
-    for images in image_files:
-        image_enhanced = Image.open(images)
-        fn = str(image_enhanced.filename)
-        brigthness_img = ImageEnhance.Brightness(image_enhanced)
-        image_enhanced = brigthness_img.enhance(brightness)
-        sharpness_img = ImageEnhance.Sharpness(image_enhanced)
-        image_enhanced = sharpness_img.enhance(sharpness)
-        contrast_img = ImageEnhance.Contrast(image_enhanced)
-        image_enhanced = contrast_img.enhance(contrast)
-        image_enhanced.show()
-        dump = fn.split("\\")
-        fn = dump[-1]
-        image_enhanced.save(f"{output_directory}{fn}")
+    
         
